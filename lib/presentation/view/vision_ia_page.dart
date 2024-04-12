0a4f18d29d14ec6b5a0d9de63ba35fd342070cbc
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-04-12 08:54:00 -0300

    feat: use image labeling

---
+++ lib/presentation/view/vision_ia_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class VisionIA extends StatelessWidget {
+  const VisionIA({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
